9b0c0f8cb6f23da23a9175b68ae1e3968037008c
Demiomad <157294157+Demiomad@users.noreply.github.com>
2026-09-09 17:38:08 +0200

    Add namespace to audio library

--- Scripts/Assets/AudioLibrary.cs
+++ Scripts/Assets/AudioLibrary.cs
@@ -1,11 +1,14 @@
 using Godot;
 using System;
 
-public partial class AudioLibrary : Node
+namespace ElevenZ.Assets
 {
-    private AudioStream _buttonDown;
-    private AudioStream _buttonUp;
+    public partial class AudioLibrary : Node
+    {
+        private AudioStream _buttonDown;
+        private AudioStream _buttonUp;
 
-    public AudioStream ButtonDown => _buttonDown ??= GD.Load<AudioStream>("res://Assets/Audio/UI/button-down.wav");
-    public AudioStream ButtonUp => _buttonUp ??= GD.Load<AudioStream>("res://Assets/Audio/UI/button-up.wav");
+        public AudioStream ButtonDown => _buttonDown ??= GD.Load<AudioStream>("res://Assets/Audio/UI/button-down.wav");
+        public AudioStream ButtonUp => _buttonUp ??= GD.Load<AudioStream>("res://Assets/Audio/UI/button-up.wav");
+    }
 }
