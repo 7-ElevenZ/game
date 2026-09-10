c95e3a75c262fed4c9717da2cd3571053c08ab7e
Demiomad <157294157+Demiomad@users.noreply.github.com>
2026-09-10 16:29:49 +0200

    Implement quit button

---
+++ Scripts/Core/GameManager.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+namespace ElevenZ.Core
+{
+    public partial class GameManager : Node
+    {
+        public static GameManager Instance { get; private set; }
+
+        public override void _Ready()
+        {
+            Instance = this;
+        }
+
+        public void QuitGame() // so i dont have to call GetTree().Quit() every time i want to quit
+        {
+            GetTree().Quit();
+        }
+    }
+}
--- Scripts/Scenes/MainMenu.cs
+++ Scripts/Scenes/MainMenu.cs
@@ -7,8 +7,30 @@ namespace ElevenZ.Scenes
 {
     public partial class MainMenu : CanvasLayer
     {
+		[ExportCategory("Audio")]
+
         [Export]
         public AudioStream MenuSong { get; set; } = AudioLibrary.MenuTheme;
+
+
+		[ExportCategory("Buttons")]
+
+		[Export]
+		public Button QuitButton { get; set; }
+
+		private void ImplementButtons()
+		{
+			if (QuitButton == null)
+			{
+				GD.PushWarning("Quit button was not attached in the Inspector.");
+				return;
+			}
+
+			if (OS.HasFeature("ios"))
+				QuitButton.Hide();
+			else
+				QuitButton.Pressed += GameManager.Instance.QuitGame;
+		}
 
         public override void _Ready()
         {
@@ -19,6 +41,8 @@ namespace ElevenZ.Scenes
             }
 
             AudioManager.Instance.PlayMusic(MenuSong);
+
+			ImplementButtons();
         }
     }
 }
